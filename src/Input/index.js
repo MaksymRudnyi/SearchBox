d2aa8cfb3b8a5f7b288d84ee06c69ec41b1bc61a
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-07-27 22:10:01 +0300

    fetch data from wikipedia

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,28 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Autocomplete from 'react-autocomplete';
+
+import axios from 'axios';
 
 const Input = () => {
     const [value, setValue] = useState('');
+    const [items, setItems] = useState([]);
+
+    const onChange = (e) => setValue(e.target.value);
+
+    useEffect(() => {
+        if (!value) {
+            return;
+        }
+
+        axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${value}&limit=5`)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                const responseItems = [];
+                const {data} = response;
+
+                for(let i = 0; i < data[1].length; i++) {
+                    responseItems.push({id: data[1][i], label: data[3][i]});
+                }
+
+                setItems(responseItems)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                setItems([])
+            });
+    }, [value]);
+
+
 
     return <Autocomplete
-        items={[
-            { id: 'foo', label: 'foo' },
-            { id: 'bar', label: 'bar' },
-            { id: 'baz', label: 'baz' },
-        ]}
-        shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+        items={items}
+        // shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
         getItemValue={item => item.label}
         renderItem={(item, highlighted) =>
             <div
                 key={item.id}
                 style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
             >
-                {item.label}
+                <a href={item.label} target="_blank">{item.id}</a>
             </div>
         }
         value={value}
-        onChange={e => setValue(e.target.value)}
-        onSelect={value => setValue( value )}
+        onChange={onChange}
+        onSelect={value => {debugger; setValue( value )}}
     />;
 };
 
